895d18806f58847f3649b80741f980d6dc43737f
T-Betrand <tebobertrand@gmail.com>
2023-09-09 07:01:37 +0100

    fixing deploy

--- basicapi/api/serializers.py
+++ basicapi/api/serializers.py
@@ -4,5 +4,5 @@ from api.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'slack_name', 'current_day', 'utc_time', 'track', 'github_file_url', 'github_repo_url', 'status_code']
+        fields = ['slack_name', 'current_day', 'utc_time', 'track', 'github_file_url', 'github_repo_url', 'status_code']
          
